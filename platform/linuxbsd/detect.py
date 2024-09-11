cc1312af22888e1eda682a6ffd30aa2bc8db4ab8
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-09-10 17:46:01 -0700

    Integrate libdatachannel 0.19.1 in the core webrtc module.
    If libdatachannel=no is used, webrtc will still compile with gdextension support.
    Supports builtin_libdatachannel=yes to use a platform installed version.
    Does not include support for libsrtp.
    
    Fix for mingw compile for Windows.
    
    Remove unused code and disable media-related functionality

--- platform/linuxbsd/detect.py
+++ platform/linuxbsd/detect.py
@@ -309,8 +309,10 @@ def configure(env: "SConsEnvironment"):
     if not env["builtin_libwebp"]:
         env.ParseConfig("pkg-config libwebp --cflags --libs")
 
-    if not env["builtin_libjpeg_turbo"]:
-        env.ParseConfig("pkg-config libturbojpeg --cflags --libs")
+    if not env["builtin_libdatachannel"]:
+        # libdatachannel does not provide a pkgconfig config yet.
+        # Goes directly into /usr/lib64 based on example RPM file in Fedora 39.
+        env.Append(LIBS=["datachannel"])
 
     if not env["builtin_mbedtls"]:
         env.ParseConfig("pkg-config mbedtls mbedcrypto mbedx509 --cflags --libs")
